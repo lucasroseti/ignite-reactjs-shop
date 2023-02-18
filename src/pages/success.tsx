cfc49c3b865e9825398ad40fc5ea8ad3904b4270
Lucas Roseti <lc.roseti@gmail.com>
2023-02-18 09:25:22 -0300

    Success layout
    Project succes layout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import { ImageSuccess, SuccessContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Product() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageSuccess></ImageSuccess>
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta Beyond</strong> the Limits já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
