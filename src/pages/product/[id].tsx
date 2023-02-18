ec5f4481c768807f2b7c9e2be9e7d0d857134cc0
Lucas Roseti <lc.roseti@gmail.com>
2023-02-18 10:06:43 -0300

    Next Head
    Next head in pages

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import Image from 'next/image'
 import Stripe from 'stripe'
 import axios from 'axios'
@@ -49,22 +50,31 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ProductImage>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ProductImage>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+      <ProductContainer>
+        <ProductImage>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ProductImage>
 
-        <p>{product.description}</p>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+          <p>{product.description}</p>
+
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleBuyProduct}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
 
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import Stripe from 'stripe'
@@ -17,20 +18,27 @@ interface SuccessProps {
 
 export default function Product({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <ImageSuccess>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageSuccess>
-      <p>
-        Uhuul <strong>{customerName}</strong>, sua{' '}
-        <strong>{product.name}</strong> the Limits já está a caminho da sua
-        casa.
-      </p>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
 
-      <Link href="/">Voltar ao catálogo</Link>
-    </SuccessContainer>
+        <ImageSuccess>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageSuccess>
+        <p>
+          Uhuul <strong>{customerName}</strong>, sua{' '}
+          <strong>{product.name}</strong> the Limits já está a caminho da sua
+          casa.
+        </p>
+
+        <Link href="/">Voltar ao catálogo</Link>
+      </SuccessContainer>
+    </>
   )
 }
 
